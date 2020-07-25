acf780b1689f5a42911449b5aef045a72b2841ff
Anak7n <buzzzzx233@gmail.com>
2020-07-25 22:22:49 +0800

    feat(type-compatibility): add rest, optional and fixed parameters code examples

--- src/10-type-compatibility.ts
+++ src/10-type-compatibility.ts
@@ -33,4 +33,21 @@ executor((message) => {
 
 outsideCancel!();
 
+// rest parameters and optional parameters
+let foo = (x: number, y: number) => {};
+let bar = (x?: number, y?: number) => {};
+let baz = (...args: number[]) => {};
+
+// 固定参数兼容可选参数和剩余参数
+foo = bar;
+foo = baz;
+
+// 可选参数不兼容固定参数和剩余参数
+// bar = foo; // not allowed, Type 'number | undefined' is not assignable to type 'number'
+// bar = baz; // not allowed
+
+// 剩余参数兼容可选参数和固定参数
+baz = foo;
+baz = bar;
+
 export {};
